6fd009aa5f88c9c38ae025c14cb8761fd2dcfca9
Sudipto Bose <Sbose562@gmail.com>
2023-11-13 19:02:35 +0530

    Updated

--- src/app/services/shortner.service.ts
+++ src/app/services/shortner.service.ts
@@ -1,20 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable ,isDevMode  } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShortnerService {
-
-  constructor(private http:HttpClient) { }
+  apiEndpoint : string = 'http://localhost:5000';
+  constructor(private http:HttpClient) { 
+    if(!isDevMode()){
+      this.apiEndpoint = 'https://urlshortner-production-37d4.up.railway.app';
+      console.log('ApiEndpoint : ' + this.apiEndpoint);
+    }
+  }
 
     getshorturl(url:string):Observable<any>{
       const body = {"url" : url};
-      return this.http.post('http://localhost:5000/api/shortner',body);
+      return this.http.post(`${this.apiEndpoint}/api/shortner`,body);
     }
 
     getVisitHistory(shortid:string):Observable<any>{
-      return this.http.get(`http://localhost:5000/api/getvistihistory/${shortid}`);
+      return this.http.get(`${this.apiEndpoint}/api/getvistihistory/${shortid}`);
     }
 }
